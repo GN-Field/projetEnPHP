fe0dd3017964acf77c5340c69b8c75d8b22644cf
ZHAN Zhun <846794521@qq.com>
2021-06-16 18:00:21 +0200

    finir model crud pour compte droit sauf la partie update

---
+++ model/compte_droit.php
@@ -0,0 +1,83 @@
+<?php
+
+    // CREATE compte droit
+    // *******************
+    function createCompteDroit($nomDroit, $paramDroit, $valeur, $idCompteUtilisateur){
+        try {
+            $db = dbConnect();
+            $mysql = "INSERT INTO comptedroit(nomDroit, paramDroit, valeur, idCompteUtilisateur)";
+            $mysql .= " VALUES(:nomDroit, :paramDroit, :valeur, :idCompteUtilisateur)";
+            $req = $db->prepare($mysql);
+            $req->bindValue(":nomDroit", $nomDroit, PDO::PARAM_STR);
+            $req->bindValue(":paramDroit", $paramDroit, PDO::PARAM_STR);
+            $req->bindValue(":valeur", $valeur, PDO::PARAM_INT);
+            $req->bindValue(":idCompteUtilisateur", $idCompteUtilisateur, PDO::PARAM_INT);
+            $req->execute();    
+        } catch (Exception $sms) {
+            echo "Ne pas pouvoir faire cette commande.", $sms->getMessage()." ".$sms->getFile()." ".$sms->getLine();
+        }
+    }
+
+
+    // READ compte droit
+    // *****************
+    function getComptesDroit(){
+        try {
+            $db = dbConnect();
+            $req = $db->query("SELECT * FROM comptedroit ORDER BY id");
+            $reponse = $req->fetchAll();
+            $req->closeCursor();
+        } catch (Exception $sms) {
+            echo "Ne pas pouvoir faire cette commande.", $sms->getMessage()." ".$sms->getFile()." ".$sms->getLine();
+        }
+        return $reponse;
+    }
+
+    function getCompteDroit($idCompteDroit){
+        try {
+            $db = dbConnect();
+            $req = $db->query("SELECT * FROM comptedroit ORDER BY id");
+            $reponse = $req->fetch();
+            $req->closeCursor();
+        } catch (Exception $sms) {
+            echo "Ne pas pouvoir faire cette commande.", $sms->getMessage()." ".$sms->getFile()." ".$sms->getLine();
+        }
+        return $reponse;
+    }
+
+
+    // UPDATE compte droit
+    // *******************
+    function updateCompteDroit($idCompteDroit, $nomDroit, $paramDroit, $valeur, $idCompteUtilisateur){
+        try {
+            $db = dbConnect();
+            $mysql = "UPDATE comptedroit SET comptedroit.nomDroit = :nomDroit, comptedroit.paramDroit = :paramDroit";
+            $mysql .= " comptedroit.valeur = :valeur, comptedroit.idCompteUtilisateur = :idCompteUtilisateur)";
+            $mysql .= " WHERE comptedroit.id = :idComptedroit";
+            $req = $db->prepare($mysql);
+            $req->bindValue(":idCompteDroit", $idCompteDroit, PDO::PARAM_INT);
+            $req->bindValue(":nomDroit", $nomDroit, PDO::PARAM_STR);
+            $req->bindValue(":paramDroit", $paramDroit, PDO::PARAM_STR);
+            $req->bindValue(":valeur", $valeur, PDO::PARAM_INT);
+            $req->bindValue(":idCompteUtilisateur", $idCompteUtilisateur, PDO::PARAM_INT);
+            $req->execute();    
+        } catch (Exception $sms) {
+            echo "Ne pas pouvoir faire cette commande.", $sms->getMessage()." ".$sms->getFile()." ".$sms->getLine();
+        }
+    }
+
+
+    // DELETE compte droit
+    // *******************
+    function deleteCompteDroit($idCompteDroit){
+        try {
+            $db = dbConnect();
+            $req = $db->prepare("DELETE FROM comptedroit WHERE comptedroit.id = :id");
+            $req->bindvalue(":id",$idCompteDroit, PDO::PARAM_INT);
+            $req->execute();
+        } catch (Exception $sms) {
+            echo "Ne pas pouvoir faire cette commande.", $sms->getMessage()." ".$sms->getFile()." ".$sms->getLine();
+        }
+    }
+
+?>
--- test.php
+++ test.php
@@ -5,7 +5,7 @@
 
 
     // *****************************
-    // require "model/annonce.php";
+    require "model/annonce.php";
     // *****************************
 
     // TEST CREATE annonce
@@ -13,13 +13,13 @@
     // createAnnonce("citroen A8", "11-06-2021 00:00:00", 60000, 1, "lorem", 1, 1);
 
 
-    // ************************************
-    // require "model/compte_utilisateur.php";
-    // ************************************
+    // ***************************************
+    require "model/compte_utilisateur.php";
+    // ***************************************
 
     // TEST CREATE compte utilisateur
     // ******************************
-    // createCompteUtilisateur("123@gmail.com", "123456789", "tito", "ti", "to", "belle photo", 1, 0000000000);
+    // createCompteUtilisateur("456@gmail.com", "123456789", "toti", "to", "ti", "photo magnifique", 1, 0000000000);
 
     // TEST READ compte utilisateur
     // ****************************
@@ -31,10 +31,35 @@
 
     // TEST UPDATE compte utilisateur
     // ******************************
-    // updateCompteUtilisateur(1, "456@gmail.com", "987654321", "toti", "to", "ti", "photo test", 1, 1212121212);
+    // updateCompteUtilisateur(3, "456@gmail.com", "987654321", "toti", "to", "ti", "photo test", 1, 1212121212);
 
     // TEST DELETE compte utilisateur
     // ******************************
     // deleteCompteUtilisateur(2);
+
+
+    // *********************************
+    require "model/compte_droit.php";
+    // *********************************
+
+    // TEST CREATE compte droit
+    // ************************
+    // createCompteDroit("respect1", "test1", 200, 3);
+
+    // TEST READ compte droit
+    // **********************
+    // $test = getComptesDroit();
+    // $test = getCompteDroit(1);
+    // echo "<pre>";
+    // var_dump($test);
+    // echo "</pre>";
+
+    // TEST UPDATE compte droit
+    // ************************
+    // updateCompteDroit(3, "respect2", "test2", 300, 3);
+
+    // TEST DELETE compte droit
+    // ************************
+    deleteCompteDroit(3);
 
 ?>
